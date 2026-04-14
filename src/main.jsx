fa3aac527c9438aff787d59dbbbebc92cc9a2d82
kdy20716-droid <kdy20716@gmail.com>
2026-04-14 17:52:55 +0900

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import "./styles.css";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />,
+  <>
+    <RouterProvider router={router} />
+    <Toaster />
+  </>,
 );
