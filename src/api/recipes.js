5e5fd952d6eb7d6bc5a0d804715db71449221041
kdy20716-droid <kdy20716@gmail.com>
2026-04-30 19:14:32 +0900

    Update recipes.js

--- src/api/recipes.js
+++ src/api/recipes.js
@@ -1,5 +1,10 @@
 const BASE_URL = "http://localhost:4000/recipes";
 
+// PUT : 전체 수정 / PATCH : 일부 수정
+// PUT과 PATCH의 차이점은 다음과 같습니다:
+// 1. PUT: 전체 리소스를 대체합니다. 즉, 클라이언트가 보낸 데이터로 기존 리소스를 완전히 덮어씌웁니다. 만약 일부 필드가 누락되면, 해당 필드는 null 또는 기본값으로 초기화될 수 있습니다.
+// 2. PATCH: 리소스의 일부를 수정합니다. 클라이언트는 변경하려는 필드만 포함하여 요청을 보냅니다. 서버는 해당 필드만 업데이트하고 나머지는 그대로 유지합니다.
+
 // 레시피 목록 조회 API
 export const getRecipes = async (searchTerm, sortOption = "latest_desc") => {
   const params = new URLSearchParams();
